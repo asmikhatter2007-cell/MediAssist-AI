72735ed8e80d647f1df0fe03db7887345ed832fa
Arushi Pandey <arushipandey@Arushis-MacBook-Air.local>
2026-07-11 21:01:26 +0530

    UI Updates

--- final_app/pages/5_Dashboard.py
+++ final_app/pages/5_Dashboard.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from navigation import render_sidebar
 
 st.set_page_config(page_title="Dashboard", page_icon="📊", layout="wide")
-render_sidebar(3)
+render_sidebar(5g)
 
 BASE_URL = "http://127.0.0.1:8000"
 
